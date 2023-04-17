e91d340f9f48c60a21975e2d4dca141144d927b1
Haresh-thumar <hareshthumar46@gmail.com>
2023-04-17 14:06:08 +0530

    thread add in component

--- src/app/thread/thread-routing.module.ts
+++ src/app/thread/thread-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SubjectThreadComponent } from './subject-thread/subject-thread.component';
 import { WebWorkersComponent } from './web-workers/web-workers.component';
-import { BlockingUiComponent } from './blocking-ui/blocking-ui.component';
+import { MultiThreadWorkerComponent } from './multi-thread-worker/multi-thread-worker.component';
 
 const routes: Routes = [
   {path: 'web-worker', component: WebWorkersComponent},
   {path: 'subject-thread', component: SubjectThreadComponent},
-  {path: 'blocking-ui', component: BlockingUiComponent},
+  {path: 'multiThreadWorker', component: MultiThreadWorkerComponent},
 ];
 
 @NgModule({
--- src/app/thread/thread.module.ts
+++ src/app/thread/thread.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ThreadRoutingModule } from './thread-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BlockingUiComponent } from './blocking-ui/blocking-ui.component';
 
 
 @NgModule({
   declarations: [
-    BlockingUiComponent
   ],
   imports: [
     CommonModule,
